f63a528449efd5be6285d23412cd3b5be959e76c
Nadiia Yuce <soyka9616@gmail.com>
2024-09-20 21:29:06 +0200

    comment create selector

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -6,6 +6,9 @@ export const selectContacts = state => state.contacts.items;
 export const selectLoading = state => state.contacts.loading;
 export const selectError = state => state.contacts.error;
 
+//? Складовий селектор викликається кожного разу, коли змінюється будь-яка властивість слайсу
+//Складовий селектор має оголошуватися через ф-ю createSelector(), яка мемоїзує виконані обчислення і задає лише потрібні залежності. Це дозволяє уникнути зайвому виклику селектора.
+//Приймає масив залежних селекторів і коллбек функцію, яка в свою чергу аргументами приймає результат виклику функцій-селекторів
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectNameFilter],
   (contacts, query) => {
